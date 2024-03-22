d80c326a52adc5b253aaf8cfe8e7adb91a97b4fb
John Forbes <john.robert.forbes@gmail.com>
2024-03-22 22:05:44 +1100

    Added ./gitter.py.

---
+++ gitter.py
@@ -0,0 +1,8 @@
+from sys import argv
+from f.gitter.main import f as main
+from hak.file.load import f as load
+from hak.file.save import f as save
+
+if __name__ == '__main__':
+  main(ask=argv[-1] != '0')
+  save('gitter.py', load('gitter.py'))
