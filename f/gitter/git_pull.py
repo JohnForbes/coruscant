b7b406894254fe3c80bea752a4d4d1495c7a9d33
John Forbes <john.robert.forbes@gmail.com>
2024-03-23 20:07:15 +1100

    Updated ./f/gitter/git_pull.py.

--- f/gitter/git_pull.py
+++ f/gitter/git_pull.py
@@ -1,7 +1,6 @@
 from hak.string.colour.bright.cyan import f as cy
 from subprocess import run as sprun
 
-# git_pull
 def f():
   print(cy("Executing 'git pull'"))
   pull_result = sprun(args=['git', 'pull'], capture_output=True, cwd='.')
