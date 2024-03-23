7517294794a2b10ba267ab560f5643416bfc6464
John Forbes <john.robert.forbes@gmail.com>
2024-03-23 22:41:50 +1100

    Added ./k/string.py.

---
+++ k/string.py
@@ -0,0 +1,42 @@
+class String:
+  def __init__(self, value: str):
+    if not isinstance(value, str):
+      raise TypeError('\n'.join([
+        'value must be of type str',
+        f'observed type: {type(value)}',
+        f'observed value: {value}'
+      ]))
+    self._value = value
+  __str__ = lambda self: str(self._value)
+  w = width = property(lambda self: len(self._value))
+
+f = lambda x: String(**x)
+
+def t():
+  from hak.pxyz import f as pxyz
+  from hak.pf import f as pf
+  def t_init_and_to_string():
+    x = {'value': 'abc'}
+    return pxyz(x, x['value'], str(f(x)))
+  if not t_init_and_to_string(): return pf('!t_init_and_to_string')
+
+  def t_w():
+    x = {'value': 'abc'}
+    return pxyz(x, len(x['value']), f(x).w)
+  if not t_w(): return pf('!t_w')
+
+  def t_type_error():
+    def t_type_error_0():
+      x = {'value': 'abc'}
+      try: f(x); return 1
+      except TypeError: return 0
+    if not t_type_error_0(): return pf('!t_type_error_0')
+    def t_type_error_1():
+      class K: pass
+      x = {'value': K()}
+      try: f(x); return 0
+      except TypeError: return 1
+    if not t_type_error_1(): return pf('!t_type_error_1')
+    return 1
+  if not t_type_error(): return pf('!t_type_error')
+  return 1
