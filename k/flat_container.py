7e8f12e31616f31b172fa08f2b191b16f7c8d819
John Forbes <john.robert.forbes@gmail.com>
2024-03-23 14:36:44 +1100

    Added ./k/flat_container.py.

---
+++ k/flat_container.py
@@ -0,0 +1,16 @@
+# FlatContainer
+# dicts:List[dict]
+
+# Flat dict of key tuples mapped to vectors
+
+# properties:
+# * record_count
+# * vector_count
+# * vectors
+# * keys_tuples
+
+# methods:
+# * data_without_zeroish
+
+f = lambda x: 1
+t = lambda: 1
