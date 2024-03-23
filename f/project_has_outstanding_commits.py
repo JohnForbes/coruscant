085d51369440ef776adf3699b9778b6ad2ef1e64
John Forbes <john.robert.forbes@gmail.com>
2024-03-23 18:53:51 +1100

    Added ./f/project_has_outstanding_commits.py.

---
+++ f/project_has_outstanding_commits.py
@@ -0,0 +1,7 @@
+# project_has_outstanding_commits
+def f():
+  from subprocess import run as sprun
+  _ = sprun(args=['git', 'status', '-s'], capture_output=True, cwd='.')
+  return len(_.stdout.decode('utf-8').split('\n')) > 1
+
+t = lambda: 1
