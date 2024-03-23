36468a2438cc599b2cede0d601627b413e382851
John Forbes <john.robert.forbes@gmail.com>
2024-03-23 13:12:29 +1100

    Updated ./f/main.py.

--- f/main.py
+++ f/main.py
@@ -1,47 +1,42 @@
 from k.block import Block
+from typing import List
 
-def f(x: dict) -> Block:
-  if not isinstance(x, dict): raise TypeError('\n'.join([
-    'x is expected to be of type dict',
-    f'observed type: {type(x)}'
-    f'observed value: {x}'
+def f(dicts: List[dict], align: str='right') -> Block:
+  if not isinstance(dicts, list): raise TypeError('\n'.join([
+    'dicts is expected to be of type list',
+    f'observed type: {type(dicts)}',
+    f'observed value: {dicts}'
   ]))
-  if not x: return Block()
-  k = list(x.keys())[0] 
-  v = x[k]
-  return Block([k, '-', v])
+  if len(dicts) == 0: return Block()
+  if dicts[0] == {}: return Block()
+  return Block([
+    list(dicts[0].keys())[0],
+    '-',
+    *[d[list(dicts[0].keys())[0]] for d in dicts]
+  ])
 
 def t():
   from hak.pf import f as pf
   from hak.pxyz import f as pxyz
   from hak.pxyf import f as pxyf
-  if not pxyf({}, Block(), f): return pf('!t_simplest')
+  if not pxyf([{}], Block(), f): return pf('!t_simplest')
 
   def t_a():
-    from f.random.string import f as rand_str
-    k = rand_str(max_length=1)
-    from random import randint; v = str(randint(a=0, b=9))
-    x = {k: v}
-    y = Block([k, '-', v])
-    z = f(x)
-    return pxyz(x, [y], [z])
+    x = [{'a': 1}, {'a': 2}]
+    y = Block(['a', '-', '1', '2'])
+    return pxyf(x, y, f, new_line=1)
   if not t_a(): return pf('!t_a')
 
-  def t_throw_if_not_dict():
-    def t_throw_if_not_dict_0():
-      x = 'blergh'
-      y = 'x is expected to be of type dict'
-      try:
-        f(x)
-      except TypeError as te:
-        if y not in str(te): return pf('y not in te')
-      return 1
-    if not t_throw_if_not_dict_0(): return pf('!t_throw_if_not_dict_0')
-    def t_throw_if_not_dict_1():
-      x = {'blergh': 'blargh'}
-      f(x)
-      return 1
-    if not t_throw_if_not_dict_1(): return pf('!t_throw_if_not_dict_1')
-    return 1
-  if not t_throw_if_not_dict(): return pf('!t_throw_if_not_dict')
+  def t_type_error():
+    x = 'a'
+    y = 'dicts is expected to be of type list'
+    z = None
+    try: z = f(x)
+    except TypeError as e:
+      z = str(e)
+      if y in z: return 1
+      return pxyz(x, y, z)
+    return pxyz(x, y, z)
+  if not t_type_error(): return pf('!t_type_error')
+
   return 1
