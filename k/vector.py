5507065ef1aa26c855412be12846d1ddf63457bd
John Forbes <john.robert.forbes@gmail.com>
2024-03-23 21:10:25 +1100

    Updated ./k/vector.py.

--- k/vector.py
+++ k/vector.py
@@ -3,7 +3,7 @@ from f.obj.is_zeroish import f as is_zeroish
 
 class Vector:
   def __init__(self, values: List): self._values = values
-  all_are_zeroish = property(lambda self: all([is_zeroish(_) for _ in self.v]))
+  is_zeroish = property(lambda self: all([is_zeroish(_) for _ in self.v]))
   v = values = property(lambda self: self._values)
   h = height = property(lambda self: len(self._values))
   t = type = property(lambda self: type(self._values[0]))
@@ -15,15 +15,17 @@ class Vector:
   u = unit = unit_str = property(lambda self: self._get_unit_str())
   w = width = property(lambda self: max([len(str(_)) for _ in self._values]))
 
+  __eq__ = lambda left, right: left.values == right.values
+
 f = lambda x: Vector(**x)
 
 def t():
   from hak.pf import f as pf
   from hak.pxyz import f as pxyz
-  def t_a():
+  def t_values():
     x = {'values': [0, 1, 2]}
     return pxyz(x, [0, 1, 2], f(x).values)
-  if not t_a(): return pf('!t_a')
+  if not t_values(): return pf('!t_values')
 
   def t_w():
     x = {'values': [0, 10, 100]}
@@ -45,15 +47,22 @@ def t():
     return pxyz(x, 4, f(x).h)
   if not t_h(): return pf('!t_h')
 
-  def t_all_are_zeroish():
-    def t_all_are_zeroish_0():
+  def t_is_zeroish():
+    def t_is_zeroish_0():
       x = {'values': [0, 0, 1]}
-      return pxyz(x, 0, f(x).all_are_zeroish)
-    if not t_all_are_zeroish_0(): return pf('!t_all_are_zeroish_0')
-    def t_all_are_zeroish_1():
+      return pxyz(x, 0, f(x).is_zeroish)
+    if not t_is_zeroish_0(): return pf('!t_is_zeroish_0')
+    def t_is_zeroish_1():
       x = {'values': [0, 0, None]}
-      return pxyz(x, 1, f(x).all_are_zeroish)
-    if not t_all_are_zeroish_1(): return pf('!t_all_are_zeroish_1')
+      return pxyz(x, 1, f(x).is_zeroish)
+    if not t_is_zeroish_1(): return pf('!t_is_zeroish_1')
     return 1
-  if not t_all_are_zeroish(): return pf('!t_all_are_zeroish')
+  if not t_is_zeroish(): return pf('!t_is_zeroish')
+
+  def t_eq():
+    t_eq_0 = lambda: f({'values': [0, 10, 100]}) != f({'values': [0, 10, 101]})
+    if not t_eq_0(): return pf('!t_eq_0')
+    t_eq_1 = lambda: f({'values': [0, 10, 100]}) == f({'values': [0, 10, 100]})
+    if not t_eq_1(): return pf('!t_eq_1')
+  if not t_eq(): return pf('!t_eq')
   return 1
