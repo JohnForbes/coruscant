453a2e7f29b25d2bad4d4dc81bdb3c45e39ba94c
John Forbes <john.robert.forbes@gmail.com>
2024-03-24 19:29:36 +1100

    Updated ./f/directory/to_dependency_graph.py.

--- f/directory/to_dependency_graph.py
+++ f/directory/to_dependency_graph.py
@@ -29,10 +29,45 @@ def f(filepaths):
   return dependencies
 
 def t():
-  # from hak.directory.filepaths.get import f as get_filepaths
-  # x = get_filepaths(filepaths=[], root='./f')
-  # y = {}
-  # z = f(x)
-  # from hak.pxyz import f as pxyz
-  # return pxyz(x, y, z)
-  return 1
+  from hak.directory.filepaths.get import f as get_filepaths
+  def up():
+    from hak.directory.make import f as mkdir
+    _temp_dir = '../_temp'
+    mkdir(_temp_dir)
+    from hak.file.save import f as save
+    _files = {
+      'a.py': '\n'.join([
+        'from .b import f as f_b',
+        'from .c import f as f_c',
+        'f = lambda x: f_b(x) + f_c(x)',
+        't = lambda: 1',
+        ''
+      ]),
+      'b.py': '\n'.join([
+        'from .c import f as f_c',
+        'f = lambda x: f_c(x)',
+        't = lambda: 1',
+        ''
+      ]),
+      'c.py': '\n'.join([
+        'f = lambda x: x+1',
+        't = lambda: 1',
+        ''
+      ])
+    }
+    for _ in _files: save(f'{_temp_dir}/{_}', _files[_])
+    return _temp_dir
+  temp_dir = up()
+  x = get_filepaths(filepaths=[], root=temp_dir)
+  y = {
+    f'{temp_dir}/a.py': {f'{temp_dir}/c.py', f'{temp_dir}/b.py'},
+    f'{temp_dir}/c.py': set(),
+    f'{temp_dir}/b.py': {f'{temp_dir}/c.py'}
+  }
+  z = f(x)
+  def dn(temp_dir):
+    from hak.directory.remove import f as remove_dir
+    remove_dir(temp_dir)
+  dn(temp_dir)
+  from hak.pxyz import f as pxyz
+  return pxyz(x, y, z)
