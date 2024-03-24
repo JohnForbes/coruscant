4a2323424a06ae00a3118293ff373983202139af
John Forbes <john.robert.forbes@gmail.com>
2024-03-24 19:06:42 +1100

    Removed ./f/block/is_a.py.

--- f/block/is_a.py
+++
@@ -1,9 +0,0 @@
-from k.block import Block as B
-from k.alignment import Alignment as A
-
-# is_block
-f = lambda b: isinstance(b, B)
-t = lambda: all([
-  f(B(['---', '   ', '---'], alignment=A('centre'))),
-  not f(['---', '   ', '---']),
-])
