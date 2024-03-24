3cb21c177f7ed333bdbc36a3c50826d9db7bb307
John Forbes <john.robert.forbes@gmail.com>
2024-03-24 15:32:34 +1100

    Added ./k/filepath.py.

---
+++ k/filepath.py
@@ -0,0 +1,22 @@
+from k.string import String
+class Filepath(String):
+  def __init__(self, value: str):
+    super().__init__(value)
+
+  file_name = property(lambda self: self._v.split('/')[-1])
+  file_extension = property(lambda self: self._v.split('.')[-1])
+  directory = property(lambda self: '/'.join(self._v.split('/')[:-1]))
+  parent_directory = property(lambda self: '/'.join(self._v.split('/')[:-2]))
+  absolute_path = property(lambda self: self._v)
+  
+  def _get_exists(self): from os.path import exists; return exists(self._v)
+  exists = property(lambda self: self._get_exists)
+  does_not_exist = property(lambda self: not self.exists)
+
+f = lambda x: Filepath(**x)
+
+def t():
+  # TODO: Test is insufficient
+  from hak.pxyz import f as pxyz
+  x = {'value': 'abc'}
+  return pxyz(x, x['value'], str(f(x)))
