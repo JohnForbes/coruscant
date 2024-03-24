8aa6b1ad42e2e2b085ad7014ca031d979709e90c
John Forbes <john.robert.forbes@gmail.com>
2024-03-24 22:44:51 +1100

    Updated ./k/parent_block.py.

--- k/parent_block.py
+++ k/parent_block.py
@@ -1,14 +1,15 @@
-from k.block import Block
-from k.blocks import Blocks
+from k.block import Block as B
+from k.blocks import Blocks as Bs
 
-class ParentBlock(Block):
-  def __init__(self, name: str, blocks: Blocks, blocks_margin: int=1):
-    if not isinstance(blocks, Blocks):
+class ParentBlock(B):
+  def __init__(self, name: str, blocks: Bs, blocks_margin: int=1):
+    if not isinstance(blocks, Bs):
       raise TypeError(f'blocks must be Blocks, got {type(blocks)}')
   
     b_base = blocks.hstack(margin=blocks_margin)
-    b_name = Block([name])
-    b = Blocks([b_name, b_base]).vstack()
+    b_name = B([name])
+    b = Bs([b_name, b_base]).vstack()
+    b = b.carry_down_vertical_lines()
     super().__init__(b.lines)
 
     self._name = name
@@ -22,52 +23,53 @@ def t():
   from hak.pf import f as pf
   from hak.pxyz import f as pxyz
   def t_one_child_column():
-    x = {
-      'name': 'name',
-      'blocks': Blocks([Block(['a', 'c', 'd'])])
-    }
-    y = Block(lines=[
-      'name',
-      '----',
-      'a',
-      'c',
-      'd',
-    ])
-    z = f(x)
-    return pxyz(x, [str(y)], [str(z)])
-  if not t_one_child_column(): return pf('!t_a')
+    x = {'name': 'name', 'blocks': Bs([B(['a', 'c', 'd'])])}
+    y = B(lines=['name', '----', 'a', 'c', 'd'])
+    return pxyz(x, [str(y)], [str(f(x))])
+  if not t_one_child_column(): return pf('!t_one_child_column')
   def t_two_child_columns():
-    x = {
-      'name': 'name',
-      'blocks': Blocks([Block(['a', 'c', 'd']), Block(['b', 'e'])])
-    }
-    y = Block(lines=[
-      'name ',
-      '-----',
-      'a | b',
-      'c | e',
-      'd |  ',
-    ])
-    z = f(x)
-    return pxyz(x, [str(y)], [str(z)])
-  if not t_two_child_columns(): return pf('!t_a')
+    x = {'name': 'name', 'blocks': Bs([B(['a', 'c', 'd']), B(['b', 'e'])])}
+    y = B(lines=['name ', '-----', 'a | b', 'c | e', 'd |  '])
+    return pxyz(x, [str(y)], [str(f(x))])
+  if not t_two_child_columns(): return pf('!t_two_child_columns')
+  def t_dog_blocks():
+    x = {'name': 'dog', 'blocks': Bs([B(['ab', 'de', 'gh'])])}
+    y = B(lines=['dog', '---', 'ab', 'de', 'gh'])
+    return pxyz(x, [str(y)], [str(f(x))])
+  if not t_dog_blocks(): return pf('!t_dog_blocks')
   def t_to_three_columns():
     x = {
       'name': 'name',
-      'blocks': Blocks([
-        Block(['a', 'd', 'g']),
-        Block(['b', 'e', 'h']),
-        Block(['c', 'f', 'i'])
-      ])
+      'blocks': Bs([B(['a', 'd', 'g']), B(['b', 'e', 'h']), B(['c', 'f', 'i'])])
     }
-    y = Block(lines=[
+    y = B(lines=[
       'name',
       '---------',
       'a | b | c',
       'd | e | f',
       'g | h | i',
     ])
-    z = f(x)
-    return pxyz(x, [str(y)], [str(z)])
-  if not t_to_three_columns(): return pf('!t_b')
+    return pxyz(x, [str(y)], [str(f(x))])
+  if not t_to_three_columns(): return pf('!t_to_three_columns')
+  def t_nested():
+    _dog_blocks = Bs([B(['ab', 'de', 'gh'])])
+    _cat_blocks = Bs([B(['ab', 'de', 'gh']), B(['c', 'f', 'i'])])
+    _fish_blocks = Bs([B(['a', 'b']), B(['d', 'e']), B(['g', 'h'])])
+    _blocks = Bs([
+      ParentBlock(name='dog', blocks=_dog_blocks),
+      ParentBlock(name='cat', blocks=_cat_blocks),
+      ParentBlock(name='fish', blocks=_fish_blocks)
+    ])
+    x = {'name': 'nested', 'blocks': _blocks}
+    y = B(lines=[
+      '         nested         ',
+      '------------------------',
+      'dog |  cat   |   fish   ',
+      '--- | ------ | ---------',
+      'ab  | ab | c | a | d | g',
+      'de  | de | f | b | e | h',
+      'gh  | gh | i |   |   |  ',
+    ])
+    return pxyz(x, str(y), str(f(x)), new_line=1)
+  if not t_nested(): return pf('!t_nested')
   return 1
