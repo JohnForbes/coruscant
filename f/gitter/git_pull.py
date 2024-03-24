22affb46d17ce01bb2aa0be25cde2c7e64e00bb2
John Forbes <john.robert.forbes@gmail.com>
2024-03-24 13:18:53 +1100

    Updated ./f/gitter/git_pull.py.

--- f/gitter/git_pull.py
+++ f/gitter/git_pull.py
@@ -1,4 +1,4 @@
-from hak.string.colour.bright.cyan import f as cy
+from f.string.to_cy import f as cy
 from subprocess import run as sprun
 
 def f():
@@ -6,4 +6,10 @@ def f():
   pull_result = sprun(args=['git', 'pull'], capture_output=True, cwd='.')
   print(cy('pull_result.stdout.decode():')+f' {pull_result.stdout.decode()}')
 
-t = lambda: 1
+def t():
+  """
+    Not writing this test yet as it is a peripheral piece of code relative to
+    the project: 'coruscant',
+    it may be moved to an external library at a later date.
+  """
+  return 1
