c96ee078829d49d9640a3da467a5ba87efd64e01
John Forbes <john.robert.forbes@gmail.com>
2024-03-24 12:17:30 +1100

    Added ./f/filepath_and_content/save.py.

---
+++ f/filepath_and_content/save.py
@@ -0,0 +1,43 @@
+from os.path import exists
+from os import remove
+from os import mkdir
+from os import rmdir
+
+from f.obj.nop import f as nop
+
+def f(filepath: str, content: str):
+  with open(filepath, 'w') as φ:
+    φ.write(content)
+    return content
+
+def t():
+  from hak.pf import f as pf
+  _root = './temp_save_string'
+  _filepath = f'{_root}/temp.txt'
+  up = lambda: (mkdir if not exists(_root) else nop)(_root)
+  up()
+
+  x_content = 'apple'
+  result = f(_filepath, x_content)
+
+  def dn():
+    if exists(_filepath): remove(_filepath)
+    rmdir(_root)
+
+  if not exists(_filepath):
+    dn()
+    return pf(f'{_filepath} file was not created by file.write.run()')
+
+  with open(_filepath, 'r') as _file:
+    file_content = _file.read()
+
+  if file_content != x_content:
+    dn()
+    return pf(f'{file_content} != {x_content}')
+
+  if result != x_content:
+    dn()
+    return pf(f'{result} != {x_content}')
+
+  dn()
+  return 1
