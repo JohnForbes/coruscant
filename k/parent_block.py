ca2775cfb11fa78ce5bb7c25943af53444eb2044
John Forbes <john.robert.forbes@gmail.com>
2024-03-24 20:37:12 +1100

    Updated ./k/parent_block.py.

--- k/parent_block.py
+++ k/parent_block.py
@@ -1,80 +1,73 @@
-from typing import List
 from k.block import Block
+from k.blocks import Blocks
 
 class ParentBlock(Block):
-  def __init__(
-    self,
-    name: str,
-    blocks: List[Block]=[],
-    do_upper_border: bool=False,
-    do_lower_border: bool=False,
-    do_left_border: bool=False,
-    do_right_border: bool=False,
-    do_vertical_lines: bool=False,
-    do_horizontal_line_below_name: bool=True
-  ):
-    # Create lines object out of blocks data
-    super().__init__([])
+  def __init__(self, name: str, blocks: Blocks, blocks_margin: int=1):
+    if not isinstance(blocks, Blocks):
+      raise TypeError(f'blocks must be Blocks, got {type(blocks)}')
+  
+    b_base = blocks.hstack(margin=blocks_margin)
+    b_name = Block([name])
+    b = Blocks([b_name, b_base]).vstack()
+    super().__init__(b.lines)
 
-    # guarantee name is of correct type
-    # guarantee name has valid value
     self._name = name
-
-    # guarantee blocks is of correct type
-    # guarantee blocks has valid value
     self._blocks = blocks
-
-    # guarantee do_upper_border is of correct type
-    # guarantee do_upper_border has valid value
-    self._do_upper_border = do_upper_border
-
-    # guarantee do_lower_border is of correct type
-    # guarantee do_lower_border has valid value
-    self._do_lower_border = do_lower_border
-
-    # guarantee do_left_border is of correct type
-    # guarantee do_left_border has valid value
-    self._do_left_border = do_left_border
-
-    # guarantee do_right_border is of correct type
-    # guarantee do_right_border has valid value
-    self._do_right_border = do_right_border
-
-    # guarantee do_vertical_lines is of correct type
-    # guarantee do_vertical_lines has valid value
-    self._do_vertical_lines = do_vertical_lines
-
-    # guarantee do_horizontal_line_below_name is of correct type
-    # guarantee do_horizontal_line_below_name has valid value
-    self._do_horizontal_line_below_name = do_horizontal_line_below_name
 
   block_count = property(lambda self: self.len(self._blocks))
 
 f = lambda x: ParentBlock(**x)
 
 def t():
-  # x = {
-  #   'name': None,
-  #   'blocks': None,
-  #   'do_upper_border': None,
-  #   'do_lower_border': None,
-  #   'do_left_border': None,
-  #   'do_right_border': None,
-  #   'do_vertical_lines': None,
-  #   'do_horizontal_line_below_name': None
-  # }
-  # Create block expectation to properly test intended functionality
-  # y = Block()
-  # z = f(x)
-  # from hak.pxyz import f as pxyz
-  # return pxyz(x, y, z)
-
-  # t_name
-  # t_blocks
-  # t_do_upper_border
-  # t_do_lower_border
-  # t_do_left_border
-  # t_do_right_border
-  # t_do_vertical_lines
-  # t_do_horizontal_line_below_name
+  from hak.pf import f as pf
+  from hak.pxyz import f as pxyz
+  def t_one_child_column():
+    x = {
+      'name': 'name',
+      'blocks': Blocks([Block(['a', 'c', 'd'])])
+    }
+    y = Block(lines=[
+      'name',
+      '----',
+      'a',
+      'c',
+      'd',
+    ])
+    z = f(x)
+    return pxyz(x, [str(y)], [str(z)])
+  if not t_one_child_column(): return pf('!t_a')
+  def t_two_child_columns():
+    x = {
+      'name': 'name',
+      'blocks': Blocks([Block(['a', 'c', 'd']), Block(['b', 'e'])])
+    }
+    y = Block(lines=[
+      'name ',
+      '-----',
+      'a | b',
+      'c | e',
+      'd |  ',
+    ])
+    z = f(x)
+    return pxyz(x, [str(y)], [str(z)])
+  if not t_two_child_columns(): return pf('!t_a')
+  def t_to_three_columns():
+    x = {
+      'name': 'name',
+      'blocks': Blocks([
+        Block(['a', 'd', 'g']),
+        Block(['b', 'e', 'h']),
+        Block(['c', 'f', 'i'])
+      ])
+    }
+    y = Block(lines=[
+      'name',
+      '---------',
+      'a | b | c',
+      'd | e | f',
+      'g | h | i',
+    ])
+    z = f(x)
+    return pxyz(x, [str(y)], [str(z)])
+  if not t_to_three_columns(): return pf('!t_b')
   return 1
