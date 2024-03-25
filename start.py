ebb4b0a3f82bd72aa7507c6c18c66ee83d3d6cee
John Forbes <john.robert.forbes@gmail.com>
2024-03-25 19:10:43 +1100

    Updated ./start.py.

--- start.py
+++ start.py
@@ -1,3 +1,29 @@
 from f.main import f as main
-x = [{'a': {'b': 1, 'c': 1}}, {'a': {'b': 2, 'c': 2}}]
+x = [
+  {
+    'plants': {
+      'fruits': {'banana': 'yum', 'cherry': 2, 'durian': 3},
+      'vegetables': {'eggplant': 1, 'fennel': 2, 'ginger': 3}
+    }
+  },
+  {
+    'plants': {
+      'fruits': {'banana': 'delicious', 'cherry': 5, 'durian': 6},
+      'vegetables': {'eggplant': 4, 'fennel': 5, 'ginger': 6}
+    }
+  },
+]
+y = """
+                         plants                         
+--------------------------------------------------------
+          fruits            |         vegetables        
+--------------------------- | --------------------------
+ banana   | cherry | durian | eggplant | fennel | ginger
+--------- | ------ | ------ | -------- | ------ | ------
+   str    |  int   |  int   |   int    |  int   |  int  
+--------- | ------ | ------ | -------- | ------ | ------
+   yum    |   2    |   3    |    1     |   2    |   3   
+delicious |   5    |   6    |    4     |   5    |   6   
+"""
+
 print(main(x))
