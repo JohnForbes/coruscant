f1d5c34dddaacd3012caf4011e1c0bd761ec8c39
John Forbes <john.robert.forbes@gmail.com>
2024-03-25 13:29:33 +1100

    Updated ./f/main.py.

--- f/main.py
+++ f/main.py
@@ -2,6 +2,29 @@ from k.block import Block
 from typing import List
 from k.flat_container import FlatContainer as FC
 from k.parent_block import ParentBlock as PB
+from f.dict.flat.nest import f as nest
+from k.leaf_block import LeafBlock as LB
+from k.blocks import Blocks as Bs
+from k.block import Block as B
+
+def _accumulate(x, path_so_far=['α']):
+  if isinstance(x, dict):
+    _blocks = Bs(blocks=[])
+    for k in x:
+      if isinstance(x[k], B):
+        _blocks.append(x[k])
+      else:
+        _blocks.append(_accumulate(x[k], path_so_far + [k]))
+    return PB(tuple(path_so_far), 1, _blocks)
+  else:
+    if isinstance(x, B):
+      return x
+    else:
+      raise TypeError('\n'.join([
+        'x is expected to be either a Block or a dict',
+        f'observed type: {type(x)}',
+        f'observed value: {x}'
+      ]))
 
 def f(dicts: List[dict]) -> Block:
   if not isinstance(dicts, list): raise TypeError('\n'.join([
@@ -11,15 +34,8 @@ def f(dicts: List[dict]) -> Block:
   ]))
   if len(dicts) == 0: return Block()
   if dicts[0] == {}: return Block()
-  
-  fc = FC(dicts)
-  if fc.addresses_max_length == 1: return fc.leaf_blocks[0]
-  if fc.addresses_max_length == 2: return PB(
-    address=fc.leaf_blocks[0].ad[:-1],
-    blocks_margin=1,
-    blocks=fc.leaf_blocks,
-  )
-  return ':('
+  d = nest(FC(dicts).leaf_blocks_as_flat_dict)
+  return B(lines=_accumulate(d).lines[2:])
 
 def t():
   from hak.pf import f as pf
@@ -30,7 +46,14 @@ def t():
   def t_a():
     x = [{'a': 1}, {'a': 2}]
     y = Block([' a ', '---', 'int', '---', ' 1 ', ' 2 '])
-    return pxyf(x, y, f, new_line=1)
+    z = f(x)
+    if z is None: return pf([
+      'z is None',
+      f'x: {x}',
+      f'[y]: {[y]}',
+      f'[z]: {[z]}'
+    ])
+    return pxyz(x, y, z, new_line=1)
   if not t_a(): return pf('!t_a')
 
   def t_type_error():
@@ -57,26 +80,28 @@ def t():
       ' 1  |  1 ',
       ' 2  |  2 ',
     ])
-    return pxyf(x, y, f, new_line=1)
+    z = f(x)
+    return pxyz(x, y, z, new_line=1)
   if not t_nested_2_levels(): return pf('!t_nested_2_levels')
 
-  # def t_nested_3_levels():
-  #   x = [
-  #     {'a': {'b': {'c': 1, 'd': 2}, 'e': {'g': 3, 'h': 4}}},
-  #     {'a': {'b': {'c': 5, 'd': 6}, 'e': {'g': 7, 'h': 8}}}
-  #   ]
-  #   y = Block([
-  #     '          a          ',
-  #     '---------------------',
-  #     '    b     |    e     ',
-  #     '----------|----------',
-  #     ' c  |  d  |  g  |  h ',
-  #     '--- | --- | --- | ---',
-  #     'int | int | int | int',
-  #     '--- | --- | --- | ---',
-  #     ' 1  |  2  |  3  |  4 ',
-  #     ' 5  |  6  |  7  |  8 ',
-  #   ])
-  #   return pxyf(x, y, f, new_line=1)
-  # if not t_nested_3_levels(): return pf('!t_nested_3_levels')
+  def t_nested_3_levels():
+    x = [
+      {'a': {'b': {'c': 1, 'd': 2}, 'e': {'g': 3, 'h': 4}}},
+      {'a': {'b': {'c': 5, 'd': 6}, 'e': {'g': 7, 'h': 8}}}
+    ]
+    y = Block([
+      '          a          ',
+      '---------------------',
+      '    b     |     e    ',
+      '--------- | ---------',
+      ' c  |  d  |  g  |  h ',
+      '--- | --- | --- | ---',
+      'int | int | int | int',
+      '--- | --- | --- | ---',
+      ' 1  |  2  |  3  |  4 ',
+      ' 5  |  6  |  7  |  8 ',
+    ])
+    z = f(x)
+    return pxyz(x, y, z, new_line=1)
+  if not t_nested_3_levels(): return pf('!t_nested_3_levels')
   return 1
